ca10b82f55ec580f42a5e9bceb6e0ba03893a739
Snick2040 <nick@Nicks-MacBook-Pro.local>
2013-11-02 14:21:32 +0000

    Initial Commit

---
+++ MostPopularYouTube/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MostPopularYouTube
+//
+//  Created by Nick on 11/2/13.
+//  Copyright (c) 2013 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MostPopularYouTube/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MostPopularYouTube
+//
+//  Created by Nick on 11/2/13.
+//  Copyright (c) 2013 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
